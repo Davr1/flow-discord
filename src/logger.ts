74b6a45abb86e6519b5a9caf3d37dda9116240b6
Davri <42148912+Davr1@users.noreply.github.com>
2024-04-28 14:13:35 +0200

    0.1.0

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,7 @@
-import { appendFileSync } from "fs";
-
 const logFile: string = "latest.log";
 
 export class Logger {
     static log(value: any) {
-        appendFileSync(logFile, JSON.stringify(value) + "\n");
+        //appendFileSync(logFile, JSON.stringify(value) + "\n");
     }
 }
